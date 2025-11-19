67b7b91f5e30b87768940b2c762b20039b6376b5
Jermaine Forbes <jermaine0forbes@gmail.com>
2025-11-18 23:03:00 -0500

    creating the profile page

--- app/view/src/pages/Profile.tsx
+++ app/view/src/pages/Profile.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
-
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { Grid } from '@mui/material';
 
 export default function Profile()
 {
     return (
-        <section>
-            this is the profile page
-        </section>
+        <Box
+            component="main"
+            id="profile"
+        >
+         <Container
+         component={Grid}
+         container
+         maxWidth="xl"
+         id="profile-grid"
+         > 
+         <Grid size={4}></Grid>
+         <Grid size="auto">
+            <section>
+                this is the profile page
+            </section>
+
+
+         </Grid>
+         </Container>
+        </Box>
     )
 }
