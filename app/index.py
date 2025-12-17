5cbde0473f2fc8aec0f03f19d00d667bf80a4b6b
Jermaine Forbes <jermaine0forbes@gmail.com>
2025-12-16 21:33:20 -0500

    connecting Mongo to app pt2

--- app/index.py
+++ app/index.py
@@ -7,6 +7,7 @@ from flask_cors import CORS
 from sqlalchemy import create_engine, text
 from sqlalchemy_utils import database_exists
 from .models.sqlalchemy.user import Base
+import pymongo
 
 
 app = Flask(__name__, template_folder="view/build", static_folder="view/build/static")
@@ -18,20 +19,37 @@ app.config['BCRYPT'] = bcrypt
 def init_db():
     print("attempting to connect")
     DB_URL = f"{Config.ALCHEMY_DATABASE_URI}/{Config.DB_NAME}"
-    try:
-        if Config.ALCHEMY_DATABASE_URI is not None:
-            if not database_exists(DB_URL):
-                engine = create_engine(Config.ALCHEMY_DATABASE_URI, echo=True)
-                with engine.connect() as connection:
-                    connection.execute(text(f"CREATE DATABASE IF NOT EXISTS {Config.DB_NAME}"))
-                    connection.commit()
+    IS_MONGO_ON = Config.MONGO_ON
+    if IS_MONGO_ON:
+
+        try:
+            if Config.MONGO_CONNECTION is not None:
+                client = pymongo.MongoClient(Config.MONGO_CONNECTION)
+                if  Config.DB_NAME is not None:
+                    db = client[Config.DB_NAME]
                     print(f"database {Config.DB_NAME}, has been created!")
-            engine = create_engine(DB_URL, echo=True)
-            Base.metadata.create_all(engine)
-            app.config['ENGINE'] = engine
-            print("database has been connected")
-    except Exception as e:
-           print(e)
+                    collection = db["users"]
+                    if collection is not None:
+                        print(f"and collection 'users', has been created!")
+                    app.config['MONGO_DB'] = db
+
+        except Exception as e:
+            print(e)
+    else:
+        try:
+            if Config.ALCHEMY_DATABASE_URI is not None:
+                if not database_exists(DB_URL):
+                    engine = create_engine(Config.ALCHEMY_DATABASE_URI, echo=True)
+                    with engine.connect() as connection:
+                        connection.execute(text(f"CREATE DATABASE IF NOT EXISTS {Config.DB_NAME}"))
+                        connection.commit()
+                        print(f"database {Config.DB_NAME}, has been created!")
+                engine = create_engine(DB_URL, echo=True)
+                Base.metadata.create_all(engine)
+                app.config['ENGINE'] = engine
+                print("database has been connected")
+        except Exception as e:
+            print(e)
 
 
 app.register_blueprint(user_bp, url_prefix = "/user")
